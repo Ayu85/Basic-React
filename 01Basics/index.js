f14812b44df88f666a6f20fc2951f89a2347b781
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-18 18:26:58 +0530

    changes

---
+++ 01Basics/index.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", { id: "title" }, "Heading one");
+const heading2 = React.createElement("h2", null, "heading two");
+const content = React.createElement("div", null, [heading, heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(content);
