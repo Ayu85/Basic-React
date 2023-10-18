ddf82f8592afaf755d50ff526f2f762e065e1403
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-18 19:28:18 +0530

    parcel app

--- 01Basics/index.js
+++ 01Basics/index.js
@@ -1,6 +1,8 @@
-const heading = React.createElement("h1", { id: "title" }, "Heading one");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", { id: "title" }, "We are on parcel");
 const heading2 = React.createElement("h2", null, "heading two");
 const content = React.createElement("div", null, [heading, heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(content);
-
