3bddfe8961bd65d8bf9a05d0bd9c09d86871859a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-19 17:02:54 +0530

    self

--- 02Basics/app.js
+++ 02Basics/app.js
@@ -1,5 +1,8 @@
-const heading = React.createElement("h1", {}, "Namaste React");
+const courseName="Namaste React";
+const heading = React.createElement("h1", {}, courseName);
 const about = React.createElement("h2", {}, "by Akshay Saini");
-const parent = React.createElement("div", { id: "wrapper" }, [heading, about]);
+const date = new Date();
+const currentDate = date.toLocaleDateString();
+const parent = React.createElement("div", { id: "wrapper" }, [heading, about,currentDate]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
