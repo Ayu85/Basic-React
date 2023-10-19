118b4bef87589594959625f8c861f0e2604a48ef
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-19 19:34:03 +0530

    reac and dom installed

--- 02Basics/app.js
+++ 02Basics/app.js
@@ -1,8 +1,16 @@
-const courseName="Namaste React";
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+const courseName = "Namaste React";
 const heading = React.createElement("h1", {}, courseName);
 const about = React.createElement("h2", {}, "by Akshay Saini");
 const date = new Date();
 const currentDate = date.toLocaleDateString();
-const parent = React.createElement("div", { id: "wrapper" }, [heading, about,currentDate]);
+const parent = React.createElement("div", { id: "wrapper" }, [
+  heading,
+  about,
+  currentDate,
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+
